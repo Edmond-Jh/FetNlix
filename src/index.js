dab9141d621e2dadb4012de4a1a5d1d33b20b9e1
edmondw2 <mohamadfj80@yahoo.com>
2021-12-18 13:45:50 +0330

    Last Commit

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import { Provider } from "react-redux";
 import str from "./Componenets/store/store";
 import App from "./App";
 // import store from "./Componenets/store/store/";
+
+alert("وبسایت فیلتر است لطفا از فیلتر شکن استفاده نمایید");
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={str}>
